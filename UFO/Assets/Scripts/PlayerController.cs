076657ea8b85198778c2f33f90bfb0af959c4ae3
DanChiem <ddanchiem@gmail.com>
2017-10-20 19:34:09 -0400

    completed UFO tutorial

---
+++ UFO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;             //Floating point variable to store the player's movement speed.
+
+    public int count;
+    public Text CountText;
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public Text WinText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        WinText.text = "";
+        CountDisplay();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            CountDisplay();
+
+        }
+    }
+
+    private void CountDisplay()
+    {
+        CountText.text = "Count: " + count;
+        if (count >= 9)
+            WinText.text = "You WIN!";
+
+    }
+}
